50a30141a9c0019f9e5781883506cecfe5f6f309
hemant chaudhari <149584453+Chaudhari1234@users.noreply.github.com>
2025-02-12 20:17:27 +0530

    JavaScript

---
+++ sortDescending.js
@@ -0,0 +1,16 @@
+function sortDescending(arr) {
+    for (let i = 0; i < arr.length - 1; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[i] < arr[j]) {
+                let temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    return arr;
+}
+
+
+let arr = [5, 2, 9, 1, 5, 6];
+console.log(sortDescending(arr)); 
